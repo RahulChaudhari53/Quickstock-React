bc8b3ba02d7c497fb8a3baa52ddd2b9bd9e86f2b
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-11 07:16:53 +0545

    add global styles and main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRouter from "./routes/AppRouter.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Slide, ToastContainer } from "react-toastify";
+import AuthContextProvider from "./auth/authProvider.jsx";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          theme="dark"
+          transition={Slide}
+        />
+      </QueryClientProvider>
+    </AuthContextProvider>
+  </StrictMode>
+);
