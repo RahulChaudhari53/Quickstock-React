50ae36f0c6213bc4436e5e72a59701b5f2fca069
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-21 10:25:29 +0545

    modify api,js to setup axios interceptor to automaticallu attatch token to headers

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,27 @@
-import axios from 'axios';
+// src/api/api.js
+import axios from "axios";
+const API_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5050/api";
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5050/api";
+const instance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-const instance = axios.create(
-    {
-        baseURL: API_URL, 
-        headers: {
-            'Content-Type': 'application/json'
-        }
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
