db2b12c7512390281f474e50c784df41f47524b1
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-19 12:57:40 +0545

    Add create purchase page

---
+++ src/pages/CreatePurchasePage.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import PurchaseForm from "../components/purchase/PurchaseForm";
+import { useCreatePurchase } from "../hooks/usePurchase";
+import { Button } from "../components/ui/Button";
+import { ArrowLeft } from "lucide-react";
+
+export default function CreatePurchasePage() {
+  const navigate = useNavigate();
+  const { mutate: createPurchase, isPending } = useCreatePurchase();
+
+  const handleFormSubmit = (formData) => {
+    createPurchase(formData, {
+      onSuccess: () => {
+        navigate("/user/purchases");
+      },
+    });
+  };
+  return (
+    <div className="container mx-auto max-w-4xl p-6 bg-gray-900 rounded-lg shadow-xl text-white font-inter">
+      <div className="flex items-center gap-4 mb-8">
+        <Button
+          onClick={() => navigate("/user/purchases")}
+          variant="outline"
+          size="icon"
+          className="bg-gray-700 hover:bg-gray-600 border-gray-600"
+        >
+          <ArrowLeft size={20} />
+        </Button>
+        <h1 className="text-3xl font-extrabold text-indigo-400">
+          Create New Purchase Order
+        </h1>
+      </div>
+
+      <PurchaseForm onSubmit={handleFormSubmit} isPending={isPending} />
+    </div>
+  );
+}
