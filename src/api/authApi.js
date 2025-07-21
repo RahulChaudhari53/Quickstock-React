628f7f8619964d998d3c0b05c85ac5996e4d44fe
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-21 21:56:30 +0545

    add endpoints for password reset

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,3 +4,16 @@ import axios from "./api";
 export const registerUserApi = (data) => axios.post("/users/signup", data);
 
 export const loginUserApi = (data) => axios.post("/users/login", data);
+
+export const forgotPasswordApi = (data) =>
+  axios.post("/users/forgotPassword", data);
+
+export const verifyOtpApi = (data, token) =>
+  axios.post("/users/verify-otp", data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const resetPasswordApi = (data, token) =>
+  axios.post("/users/resetPassword", data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
