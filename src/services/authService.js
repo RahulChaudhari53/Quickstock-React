9cae566474ff93ac9949d2c59f98291eebf6b22f
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-21 21:58:06 +0545

    add services for password reset api's

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,11 @@
 // services/admin/authService.js
-import { loginUserApi, registerUserApi } from "../api/authApi";
+import {
+  loginUserApi,
+  registerUserApi,
+  forgotPasswordApi,
+  verifyOtpApi,
+  resetPasswordApi,
+} from "../api/authApi";
 
 export const registerUserService = async (formData) => {
   try {
@@ -16,5 +22,32 @@ export const loginUserService = async (formData) => {
     return response.data;
   } catch (err) {
     throw err.response?.data || { message: "Login Failed" };
+  }
+};
+
+export const forgotPasswordService = async (formData) => {
+  try {
+    const response = await forgotPasswordApi(formData);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: "Failed to send OTP." };
+  }
+};
+
+export const verifyOtpService = async ({ otp, token }) => {
+  try {
+    const response = await verifyOtpApi({ otp }, token);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: "Failed to verify OTP." };
+  }
+};
+
+export const resetPasswordService = async ({ newPassword, token }) => {
+  try {
+    const response = await resetPasswordApi({ newPassword }, token);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: "Failed to reset password." };
   }
 };
